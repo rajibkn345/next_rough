be2f39fc01f342aa64e34579042ad8adebe7fd3c
Roman Khan Rajib <rajibkn345@gmail.com>
2023-08-15 18:17:56 +0600

    first commit

---
+++ app/Dashboard/RootLayout.jsx
@@ -0,0 +1,12 @@
+export default function RootLayout({
+  children, // will be a page or nested layout
+}) {
+  return (
+    <section>
+      {/* Include shared UI here e.g. a header or sidebar */}
+      <nav></nav>
+
+      {children}
+    </section>
+  );
+}
